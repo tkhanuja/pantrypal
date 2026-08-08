41c1c89c7649fd3dcddb10762c8798bf82ce6fa8
tkhanuja <tanvikhanuja21@gmail.com>
2026-08-08 14:20:17 -0700

    working on firebase.ts recognizing env variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -21,8 +21,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
   appId: import.meta.env.VITE_FIREBASE_APP_ID || "",
-  firestoreDatabaseId:
-    import.meta.env.VITE_FIREBASE_FIRESTORE_DATABASE_ID || "default",
+  firestoreDatabaseId: import.meta.env.VITE_FIREBASE_FIRESTORE_DATABASE_ID || "default",
 };
 
 const app =
